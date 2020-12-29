b46477001c65f3c36b5254939824379a490b3e6e
yogocik <yogicrhama@gmail.com>
2020-12-29 14:01:30 +0700

    First

---
+++ BookListRazor/Models/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public DbSet<Books> books { get; set; }
+        protected override void OnModelBuilder(ModelBuilder modelBuilder) => modelBuilder.UseNpgsql("Host=localhost;Database=booklist;Username=postgres;Password=darticecek12");
+    }
+}
